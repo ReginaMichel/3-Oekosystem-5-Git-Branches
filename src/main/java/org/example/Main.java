4c90b46c62bf535e867c7dc64c4af6ab9ed95ade
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-07-14 09:48:48 +0200

    Added Obstsong lyrics.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,20 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        obstsong();
+    }
+    public static void obstsong(){
+        mango();
+        ananas();
+        kiwi();
+    }
+    public static void mango(){
+        System.out.println("Mango Mango Mango Mango Mango Mango Mango Mango Kirsch.");
+    }
+    public static void kiwi(){
+        System.out.println("Kiwi Kiwi Kiwi Kirsch.");
+    }
+    public static void ananas(){
+        System.out.println("Ananas Banane Ananas Kirsch.");
     }
 }
